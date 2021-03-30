47d4f779f1c87423c79b274b83da80e1b564e95c
someoneb100 <lawamulet@gmail.com>
2021-03-30 20:55:14 +0200

    Implemented command line argument parsing
    used package: optparse-applicative

--- LeGit/app/Main.hs
+++ LeGit/app/Main.hs
@@ -1,6 +1,45 @@
 module Main where
-
 import Lib
 
+import Data.List
+import Data.Monoid
+import Options.Applicative
+
+
+data Command = Greet
+             | Init {directory :: String, force :: Bool}
+  deriving (Eq, Show)
+
+
+
+optDir :: Parser String
+optDir = strOption (long "directory"
+                   <> short 'd' 
+                   <> value "."
+                   <> metavar "PATH" 
+                   <> help "Path to directory to be initialised into a repository (default: current working directory)")
+
+optForce :: Parser Bool
+optForce = switch (long "force"
+                  <> short 'f'
+                  <> help "Forces initialization (may owerwrite previous repo info and delete files named after required repo info files)")
+
+initOptions :: Parser Command
+initOptions = Init <$> optDir <*> optForce
+
+optLeGit :: Parser Command
+optLeGit = subparser (
+              command "greet" (info (pure Greet) (progDesc "Print greeting"))
+           <> command "init" (info initOptions (progDesc "Initialises a directory into a LeGit repo"))
+         )
+
+run :: Command -> IO ()
+run (Init d f) = putStrLn $ "Initializing " ++ d ++ if f then " with FORCE" else ""
+run Greet = putStrLn "Hi!"
+
+opts :: ParserInfo Command
+opts = info (optLeGit <**> helper) (fullDesc
+        <> header "LeGit - a SVC implemented in Haskell (somewhat based on git)" )
+
 main :: IO ()
-main = someFunc
+main = execParser opts >>= run
