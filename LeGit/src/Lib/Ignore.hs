c89cbb97403ef6b1919e97e11f4db453a6f8788a
milwus <petrickovicm99@gmail.com>
2021-04-12 01:44:02 +0200

    Created library for managing ignored FilePaths

---
+++ LeGit/src/Lib/Ignore.hs
@@ -0,0 +1,30 @@
+module Lib.Ignore () where
+
+import Lib.Basic
+import System.FilePath
+import Text.JSON
+
+readIgnore :: Repo -> IO JSValue
+readIgnore r = pom . decode <$> readFile (ignoreFile r)
+    where pom (Ok rez) = rez
+          pom _ = JSArray []
+          
+writeIgnore :: Repo -> JSValue -> IO () 
+writeIgnore r js = writeFile (ignoreFile r) (encode js)
+
+jsonToFilePaths :: JSValue -> [FilePath]
+jsonToFilePaths (JSArray xs) = filter (not . null) $ map pom xs
+    where pom (JSString s) = fromJSString s
+          pom _ = ""
+jsonToFilePaths _ = []
+
+filePathsToJson :: [FilePath] -> JSValue
+filePathsToJson = showJSONs
+
+-- showIgnore :: FilePath -> IO ()
+-- showIgnore fp = findRepo fp >>= pom
+--     where pom (Just r) = showRepoUserInfo r
+--           pom Nothing  = putStr "Error :: "
+--                       >> putStr fp 
+--                       >> putStrLn " can't be shown: not a repository!"
+--                       >> exitFailure
