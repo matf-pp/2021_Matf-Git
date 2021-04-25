e05917506a14245c4367229c9c9fe3aa3db4c6f5
milwus <petrickovicm99@gmail.com>
2021-04-25 19:19:58 +0200

    Implemented Tree.hs

--- LeGit/src/LeGit/Tree.hs
+++ LeGit/src/LeGit/Tree.hs
@@ -1,10 +1,10 @@
-module LeGit.Tree () where
+module LeGit.Tree (getPredecessors, insertNode) where
 
 import LeGit.Basic
 
 import System.FilePath
 import Data.Maybe
-import Text.JSON (encode, JSValue)
+import Text.JSON (encode)
 import Data.Text (unpack)
 import Text.Hex (encodeHex)
 import Crypto.Hash.SHA256 (hash)
@@ -14,7 +14,10 @@ import qualified Data.HashMap.Strict as M
 type Sha = String
 type Tree = M.HashMap Sha [Sha]
 
-shaGen :: JSValue -> String
+getTree :: Repo -> IO Tree
+getTree = readJsonFromRepo treeFile M.empty
+
+shaGen :: Commit -> Sha
 shaGen = unpack . encodeHex . hash . fromString . encode
 
 shaToFP :: Repo -> Sha -> FilePath
@@ -29,11 +32,29 @@ getParents = fmap (fromMaybe undefined) . M.lookup
 isRoot :: Sha -> Tree -> Bool
 isRoot = fmap null . getParents
 
-getPredecessors :: Sha -> Tree -> [Sha]
-getPredecessors k m
+getPredecessorsSha' :: Sha -> Tree -> [Sha]
+getPredecessorsSha' k m
     | isRoot k m = []
-    | otherwise  = getPredecessors par m ++ [par]
+    | otherwise  = par : getPredecessorsSha' par m
     where par    = head $ getParents k m
 
-insertNode :: Repo -> [Sha] -> JSValue -> IO ()
-insertNode = undefined
+getPredecessorsSha :: Sha -> Tree -> [Sha]
+getPredecessorsSha = fmap reverse . getPredecessorsSha'
+
+getPred :: Sha -> Repo -> IO Commit
+getPred s = readJsonFromRepo (flip shaToFP s) (error $ "Internal Error :: Cannot find commit " ++ s)
+
+shaToShas :: Tree -> Sha -> [Sha]
+shaToShas t = flip getPredecessorsSha t
+
+getPredecessors :: Repo -> Sha -> IO [Commit]
+getPredecessors r s = do
+    tree <- getTree r
+    mapM (flip getPred r) $ shaToShas tree s
+
+insertNode :: Repo -> Commit -> [Sha] -> IO ()
+insertNode r commit parents = do
+    let sha = shaGen commit
+    tree <- getTree r
+    writeJsonToRepo treeFile r $ M.insert sha parents tree
+    writeJsonToRepo (flip shaToFP sha) r commit
