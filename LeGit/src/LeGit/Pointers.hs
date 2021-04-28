4e528118e1662efac268ccdb9b6926300e88e777
milwus <petrickovicm99@gmail.com>
2021-04-28 19:47:04 +0200

    pointers

--- LeGit/src/LeGit/Pointers.hs
+++ LeGit/src/LeGit/Pointers.hs
@@ -1,9 +1,14 @@
 module LeGit.Pointers (initPointers) where
 
 import LeGit.Basic
+import LeGit.Tree
 import Data.Maybe
 import qualified Data.HashMap.Strict as M
 import Data.Hashable
+
+getPointers :: Repo -> IO Pointers
+getPointers = readJsonFromRepo pointersFile e
+    where e = error "Internal Error :: Failed to parse Pointers file"
 
 getShaFromHead :: Pointers -> ShaStr
 getShaFromHead (Pointers (Sha h) _ _) = h
@@ -11,6 +16,9 @@ getShaFromHead (Pointers (Ref h) r _) = fromMaybe e $ M.lookup h r
     where e = error $ "Internal Error :: Reference " ++ h ++ " does not exist"
 getShaFromHead (Pointers (Tag h) _ t) = fromMaybe e $ M.lookup h t
     where e = error $ "Internal Error :: Tag " ++ h ++ " does not exist"
+
+getShaFromRepo :: Repo -> IO ShaStr
+getShaFromRepo r = getShaFromHead <$> getPointers r
 
 insert' :: (Eq k, Hashable k) => k -> v -> M.HashMap k v -> M.HashMap k v
 insert' k v m = pom $ M.lookup k m
@@ -38,3 +46,13 @@ updateRef _ _ = error "Cannot update when Head is not reference"
 initPointers :: Repo -> ShaStr -> IO ()
 initPointers r s = writeJsonToRepo pointersFile r
                  $ Pointers (Ref "main") (M.singleton "main" s) M.empty
+
+getPredCommits :: Repo -> IO [Commit]
+getPredCommits r = getShaFromRepo r >>= getPredecessors r
+
+writeCommit :: Repo -> Commit -> IO ()
+writeCommit r c = do
+    p <- getPointers r
+    let oldS = getShaFromHead p
+    s <- insertNode r c [oldS]
+    writeJsonToRepo pointersFile r $ updateRef p s
